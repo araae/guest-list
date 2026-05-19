8206fb914549d7d00bda42ef7c0a618d2ebbe0a7
araae <araliae@icloud.com>
2026-05-19 07:55:52 -0700

    finished app and guestlist

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import GuestList from "./GuestList";
+import GuestDetails from "./GuestDetails";
+import { useGuests } from "./Context";
+
+//implement conditional if statements to check the page state value
+//render the GuestList component if the page matches list
+//render the GuestDetails component if the page matches details
+
+export default function App() {
+  const { page } = useGuests();
+
+  if (page === "LIST") {
+    return <GuestList />;
+  }
+
+  if (page === "DETAILS") {
+    return <GuestDetails />;
+  }
+}
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { GuestProvider } from "./Context.jsx";
+
+//render the GuestProvider grouping the App inside to share global states
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <GuestProvider>
+      <App />
+    </GuestProvider>
+  </StrictMode>,
+);
