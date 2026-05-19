a8cec19da7f77da99b0cfafd04c40ecf8e91d439
araae <araliae@icloud.com>
2026-05-19 07:57:25 -0700

    cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { App } from "./App.jsx";
 import { GuestProvider } from "./Context.jsx";
 
 //render the GuestProvider grouping the App inside to share global states
